bc2de01fb00451c116f1b3fae2ea90d3e815d15c
Akshay Bhopani <akshaybhopani@gmail.com>
2019-03-03 22:22:10 +0530

    Updates

---
+++ precache-manifest.b17f82d420f1885eaceec144a70c4cd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "549ade2f7f20de87675c154466962675",
+    "url": "/facerecognition/static/media/logo.549ade2f.PNG"
+  },
+  {
+    "revision": "1a73abdcfa4072db5abd",
+    "url": "/facerecognition/static/js/runtime~main.1a73abdc.js"
+  },
+  {
+    "revision": "83674eeb2a059b5f29ae",
+    "url": "/facerecognition/static/js/main.83674eeb.chunk.js"
+  },
+  {
+    "revision": "6c33d184fb83c630ca19",
+    "url": "/facerecognition/static/js/1.6c33d184.chunk.js"
+  },
+  {
+    "revision": "83674eeb2a059b5f29ae",
+    "url": "/facerecognition/static/css/main.05f114b3.chunk.css"
+  },
+  {
+    "revision": "6c33d184fb83c630ca19",
+    "url": "/facerecognition/static/css/1.6ea86d34.chunk.css"
+  },
+  {
+    "revision": "8ed8330f13727f0f015c08cf03b878a1",
+    "url": "/facerecognition/index.html"
+  }
+];
