f3ac93941edc1a3e77722e047c91d8b243d62c25
Akshay <akshaybhopani@gmail.com>
2020-04-21 20:50:30 +0530

    Updates

---
+++ precache-manifest.63c9977f82542033e451e14c14685148.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac01d318f720a0ba263c1aad16dd85f",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "f8576fc64373c92ed405",
+    "url": "/facerecognition/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "79a10627609f1e18bd14",
+    "url": "/facerecognition/static/css/main.2bde5312.chunk.css"
+  },
+  {
+    "revision": "f8576fc64373c92ed405",
+    "url": "/facerecognition/static/js/2.98c51a01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.98c51a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79a10627609f1e18bd14",
+    "url": "/facerecognition/static/js/main.c3e3d783.chunk.js"
+  },
+  {
+    "revision": "2ea3e8101697ca306707",
+    "url": "/facerecognition/static/js/runtime-main.603f1b3e.js"
+  },
+  {
+    "revision": "549ade2f7f20de87675c154466962675",
+    "url": "/facerecognition/static/media/logo.549ade2f.PNG"
+  }
+]);
